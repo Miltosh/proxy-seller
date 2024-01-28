e9b158435e0a5309581d0c9cf5ad800c51fa7ac8
Miltosh <skalinovskiy94@gmail.com>
2024-01-28 17:53:02 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Proxy Seller
+
+This project is a for Proxy Seller. It involves creating a web application that allows users to interact with squares within a field based on certain modes.
+
+## Table of Contents
+
+- [Demo](#demo)
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+- [Usage](#usage)
+- [Credits](#credits)
+
+## Demo
+
+You can view a live demo of the project [here](https://miltosh.github.io/proxy-seller/).
+
+## Features
+
+The Proxy Seller app has the following features:
+
+- ReactJS (pure)
+- Compatible with NodeJS 14 version
+- Assembling the project using WebPack
+
+As an API, was used the service https://jsonplaceholder.typicode.com/
+
+## Getting Started
+
+### Prerequisites
+
+To run this project, you will need:
+
+- A modern web browser to view the project.
+- Node.js and npm (Node Package Manager) installed on your system.
+
+## Usage
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory using the terminal.
+3. Run the following command to install the project's dependencies:
+
+   ```
+   npm install
+   ```
+
+4. Start the development server:
+
+   ```
+   npm start
+   ```
+
+5. Open your web browser and visit the URL where the development server is running (usually http://localhost:3000).
+
+## Credits
+
+This project was created by Stanislav Kalinovskiy.
---
+++ src/components/UsersTable.jsx
@@ -0,0 +1,98 @@
+import React, { useMemo } from 'react'
+import classNames from 'classnames'
+import { Link, useSearchParams } from 'react-router-dom'
+import { getSearchWith } from '../helpers/searchHelper'
+
+export const UsersTable = ({ users }) => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const currentOrder = searchParams.get('order')
+
+  const getClassNamesSort = () => {
+    return classNames('fas', {
+      'fa-sort': currentOrder === null,
+      'fa-sort-up': currentOrder === 'asc',
+      'fa-sort-down': currentOrder === 'desc',
+    })
+  }
+
+  const onChangeOrder = () => {
+    let newOrder
+
+    switch (currentOrder) {
+      case 'asc':
+        newOrder = 'desc'
+        break
+
+      case null:
+        newOrder = 'asc'
+        break
+
+      default:
+        newOrder = null
+    }
+
+    setSearchParams(getSearchWith(searchParams, { order: newOrder }))
+  }
+
+  const sortUsers = useMemo(() => {
+    switch (currentOrder) {
+      case 'asc':
+        return [...users].sort((user1, user2) => user1.username.localeCompare(user2.username))
+
+      case 'desc':
+        return [...users].sort((user1, user2) => user2.username.localeCompare(user1.username))
+
+      default:
+        return users
+    }
+  }, [currentOrder, users])
+
+  return (
+    <table className='table is-striped is-hoverable is-narrow is-fullwidth'>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+
+          <th>
+            <span
+              className='is-flex is-flex-wrap-nowrap is-clickable'
+              onClick={onChangeOrder}
+              aria-label='button'
+            >
+              Username
+              <span className='icon'>
+                <i className={getClassNamesSort()} />
+              </span>
+            </span>
+          </th>
+
+          <th>E-mail</th>
+          <th>Links to Posts</th>
+          <th>Links to Albums</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {sortUsers.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+              <a href={`mailto: ${user.email}`} className='is-link'>
+                {user.email}
+              </a>
+            </td>
+            <td>
+              <Link to={`${user.id}/posts`}>{`${user.username} Posts`}</Link>
+            </td>
+            <td>
+              <Link to={`${user.id}/albums`}>{`${user.username} Albums`}</Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
